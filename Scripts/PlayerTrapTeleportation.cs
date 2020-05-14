beffec368653c0fc205ac75f43b2fc450216d60e
Latsuj <justal.kevin@gmail.com>
2020-05-14 19:11:07 +0800

    [FEATURE] Adding the teleport for the trap

---
+++ Scripts/PlayerTrapTeleportation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrapTeleportation : MonoBehaviour
+{
+    public Transform teleportation;
+    public GameObject player;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="Player") {
+            StartCoroutine(Teleport());
+        }
+    }
+    
+    IEnumerator Teleport() {
+        yield return new WaitForSeconds(0.3f);
+        player.transform.position = new Vector2(teleportation.transform.position.x,teleportation.transform.position.y);
+    }
+}
