95b36c5a70918cec9ca8a03401207258a30d37b5
Latsuj <justal.kevin@gmail.com>
2020-05-18 01:05:03 +0800

    [FEATURE] Adding light and particule

---
+++ Scripts/PlayerUIScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerUIScript : MonoBehaviour
+{
+    public TextMeshProUGUI timer;
+    float time=0f;
+
+    void Update() {
+        time+=Time.deltaTime;
+        timer.text=FormatTime(time);
+    }
+
+    public string FormatTime( float time )
+    {
+        int minutes = (int) time / 60 ;
+        int seconds = (int) time - 60 * minutes;
+        int milliseconds = (int) (1000 * (time - minutes * 60 - seconds));
+        return string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds );
+    }
+}
