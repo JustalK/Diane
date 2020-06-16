dbf481598ed41b04cb76c058a723de0ec6ba4ee1
Latsuj <justal.kevin@gmail.com>
2020-06-16 13:00:16 +0800

    [FEATURE] Adding a transition object for glowing the scene

---
+++ Scripts/Transitions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class Transitions : MonoBehaviour
+{
+    public static Transitions instance = null;
+    private Animator animator;
+    private UnityEngine.Experimental.Rendering.Universal.Light2D light;
+    
+    void Awake() {
+        if(instance == null) {
+            instance = this;
+            return;
+        }
+        if(instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    
+    void Start() {
+        light = GetComponentInChildren<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        animator = GetComponent<Animator>();
+    }
+    
+    public void Glowing() {
+        animator.SetBool("isGlowing",true);
+    }
+}
